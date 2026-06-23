b21069e634be661e3472bf27c6c0016e6519c3b8
ever123404 <ever.math15@gmail.com>
2026-06-23 18:37:45 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Sol de Huacariz — Service Worker v5.1
 // Incrementar VERSION para forzar actualización en todos los dispositivos
-var VERSION = 'sdh-v6.1';
+var VERSION = 'sdh-v6.3';
 var ARCHIVOS = [
   './ever.html','./jorge.html','./carlos.html',
   './administrador.html','./mozos.html',
